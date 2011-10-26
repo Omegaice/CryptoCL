b9307be675f6eea46c453aa83740047a759496b8
James Sweet <james.sweet88@googlemail.com>
2011-10-26 11:59:25 -0400

    Added tests for 128bit and 192bit aes cryptography

--- test/include/AES/ReferenceTest.h
+++ test/include/AES/ReferenceTest.h
@@ -9,13 +9,21 @@ namespace AES {
 		private:
 			CPPUNIT_TEST_SUITE( ReferenceTest );
 				CPPUNIT_TEST( testRoundKeyGeneration );
-				CPPUNIT_TEST( testEncryption );
-				CPPUNIT_TEST( testDecryption );
+				CPPUNIT_TEST( testEncryption128 );
+				CPPUNIT_TEST( testEncryption192 );
+				CPPUNIT_TEST( testEncryption256 );
+				CPPUNIT_TEST( testDecryption128 );
+				CPPUNIT_TEST( testDecryption192 );
+				CPPUNIT_TEST( testDecryption256 );
 			CPPUNIT_TEST_SUITE_END();
 		public:
 			void testRoundKeyGeneration();
-			void testEncryption();
-			void testDecryption();
+			void testEncryption128();
+			void testEncryption192();
+			void testEncryption256();
+			void testDecryption128();
+			void testDecryption192();
+			void testDecryption256();
 	};
 
 }
