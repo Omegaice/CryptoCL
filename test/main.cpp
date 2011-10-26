dbd3bf13f6ff494a8f8aa9c5191256c8ff250d88
James Sweet <james.sweet88@googlemail.com>
2011-10-26 16:29:47 -0400

    Updated test runner to generate xml

--- test/main.cpp
+++ test/main.cpp
@@ -8,10 +8,22 @@
 int main( int argc, char **argv)
 {
 	CppUnit::TextUi::TestRunner runner;
-	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	CppUnit::TestResultCollector  collector;
+	CppUnit::TestResult result;
+	result.addListener(&collector);
 
-	runner.addTest( registry.makeTest() );
-	runner.run();
+	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	runner.addTest(registry.makeTest());
+
+	// Run the tests.
+	runner.run(result);
+
+
+	// writing result on a XML file
+	std::ofstream xmlFileOut("testresults.xml");
+	CppUnit::XmlOutputter xmlOut(&collector, xmlFileOut);
+	xmlOut.write();
 	
+	// Return error code 1 if the one of test failed.
 	return 0;
 }
