6de4abbb6d3577a96f5c5ca1e7ae5bbf5df8dd91
James Sweet <james.sweet88@googlemail.com>
2011-11-06 14:10:36 -0500

    Added basic constructor and virtual destructor for base Cipher

--- include/CryptoCL/Cipher.h
+++ include/CryptoCL/Cipher.h
@@ -7,6 +7,9 @@ namespace CryptoCL {
 	typedef std::vector<unsigned char> DataArray;		
 	
 	struct Cipher {
+		Cipher() {}
+		virtual ~Cipher() {}
+		
 		virtual const DataArray Encrypt( const DataArray& data ) = 0;
 		virtual const DataArray Decrypt( const DataArray& data ) = 0;
 	};
